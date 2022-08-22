b8fe353b7681eb5321d39817216b0121c4398baf
Austin Lance <austin.lance@smoothstack.com>
2022-08-22 11:48:51 -0700

    cleanup

--- test_order_db.py
+++ test_order_db.py
@@ -19,7 +19,6 @@ connection = mysql.connector.connect(host='localhost', database='leftovers', use
 def test_db():
     # Connect to MySQL database
     assert connection.is_connected() == True
-    
 
 my_cursor=connection.cursor()
 
@@ -39,8 +38,6 @@ def test_append():
         
     print(my_cursor.rowcount, "records are inserted.")
 
-
-
 def test_read():
     # read data of all the columns from the table
     query = "SELECT * FROM tbl_order"
@@ -48,7 +45,6 @@ def test_read():
     res = my_cursor.fetchall() #storing the output of fetchall() in the res
     for row in res: #printing all the records in res
         print(row)
-        
 
 def test_delete_table():
     query = "SET FOREIGN_KEY_CHECKS=0; DROP TABLE tbl_order"
